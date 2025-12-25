b79494c025bd93a67010471811182b620c971024
ars_9449 <alexmerser30111995@hotmail.com>
2025-12-25 16:13:09 +0300

    connectong sapnco

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SAP.Middleware.Connector;
 
 namespace sap_connect_net_framework
 {
@@ -10,6 +11,7 @@ namespace sap_connect_net_framework
     {
         static void Main(string[] args)
         {
+
         }
     }
 }

06a16c61f028464ba086438afd78be7915ca5eb3
ars_9449 <alexmerser30111995@hotmail.com>
2025-12-25 16:04:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sap_connect_net_framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
